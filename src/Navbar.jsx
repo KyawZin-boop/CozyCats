613a0766219cbe9a56d298ccf34e95dd7533353d
Kyaw Zin Thant <kyawzinthant57@gmail.com>
2024-10-20 21:50:32 +0630

    first commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+export default function Navbar() {
+    return( <nav className='navbar navbar-expand-lg'>
+                <div className='logoDiv'><img src="/src/assets/logo.png" alt="" /></div>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="navLinks collapse navbar-collapse" id="navbarTogglerDemo02">
+                <a href="home">Home</a>
+                <a href="#">Adoption</a>
+                <a href="#">Services</a>
+                <a href="#">Gallery</a>
+                <a href="#">Products</a>
+                <a href="#">FAQ</a>
+                <div className='search ms-3'>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} className='fs-4'/>
+                </div>
+                <div className='mobileSearch'>
+                    <input type="text" />
+                    <button>Search</button>
+                </div>
+                </div>
+            </nav>
+    )
+}
